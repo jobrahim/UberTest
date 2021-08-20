2632d8b176654093f2f0bcaf7969a43eb612c587
Ibra <jicastellanos@stefanini.com>
2021-08-20 12:39:44 -0300

    agregado pedro

--- UberBasico/src/main/java/com/Uber/UberBasico/Controller/UberControlller.java
+++ UberBasico/src/main/java/com/Uber/UberBasico/Controller/UberControlller.java
@@ -14,10 +14,13 @@ public class UberControlller {
     private CalculoCalles calculoCalles;
 
     @RequestMapping("/SolicitarRuta")
-     public String route( @RequestBody ClientAddress clientAddress){
+    public String route(@RequestBody ClientAddress clientAddress) {
 
-     return calculoCalles.returnStreet(clientAddress);
+        return calculoCalles.returnStreet(clientAddress);
 
-     }
+    }
 
 }
+// pedro perez
+
+// hola coca cola
